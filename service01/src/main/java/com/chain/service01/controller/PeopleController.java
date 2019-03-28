c04f9ca357d8d2e9e7ff9ceced5a42690acc4484
Charles <1272777053@qq.com>
2019-03-28 18:38:00 +0800

    peple

--- service01/src/main/java/com/chain/service01/controller/PeopleController.java
+++ service01/src/main/java/com/chain/service01/controller/PeopleController.java
@@ -3,18 +3,28 @@ package com.chain.service01.controller;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.chain.service01.model.PeopleMapper;
+import com.chain.service01.model.PeopleModel;
 
 @RestController
+@RequestMapping(value="people")
 public class PeopleController {
 	@Autowired
 	private PeopleMapper peopleMapper;
 	
 	@PostMapping("addName")
 	public void addName() {
-		peopleMapper.insert("xiaoming", 15, "boy", new Date());
+		
+	}
+	
+	@PostMapping("addPeople")
+	public String addPeople(@ModelAttribute PeopleModel p) {
+		peopleMapper.insert(p.getName(), p.getAge(), p.getSex(), new Date());
+		return "success";
 	}
 }
